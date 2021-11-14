46317884f054b3962ae881d297bee23525fa904c
BonesYT <63462200+BonesYT@users.noreply.github.com>
2021-11-14 12:54:02 -0300

    committing 11

---
+++ projects/music/main.js
@@ -0,0 +1,37 @@
+songs = ['Bitbase', 'Thesuren', 'Orsical', 'Ditern', 'Ultraticalic', 'Megalatic', 'Ultrimontalitic', 'Universal0', 'Ostale', 'Ultrismate 2', 'specinal']
+songnames = songs.map((v)=>{return v + '.mp3'})
+playing = []
+
+document.makeElement = (tag, innerHTML)=>{
+    var node = document.createElement(tag);
+    var textnode = document.createTextNode(innerHTML);
+    node.appendChild(textnode);
+    return node
+}
+document.placeElement = (node, id)=>{
+    document.getElementById(id).appendChild(node);
+}
+
+for (i=0; i<songs.length; i++) {
+    a = document.makeElement('button', songs[i])
+    a.id = 'Song' + i
+    document.placeElement(a, 'songs')
+    const testElement = document.getElementById('songs');
+    const lineBreak = document.createElement('br');
+    testElement.appendChild(lineBreak);
+}
+for (i=0; i<songs.length; i++) {
+    document.getElementById('Song' + i).addEventListener('click', eval('()=>{playSong('+i+')}'))
+}
+
+function playSong(id) {
+    var a = new Audio(songnames[id])
+    playing.push(a)
+    a.play()
+} 
+
+function stopAll() {
+    playing.forEach(element => {
+        element.pause()
+    });
+}

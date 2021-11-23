5523b15a5b01afaa0abfd6694860d2624e928903
BonesYT <63462200+BonesYT@users.noreply.github.com>
2021-11-23 12:44:53 -0300

    committing 24

---
+++ games/progress-rise/game.js
@@ -0,0 +1,116 @@
+EN = ExpantaNum
+
+function Game() {
+    this.points = EN(0) //points
+    this.tpoints = EN(0) //total points
+    this.bars = [new Bar] //progress bars
+    this.next = EN(200) //progress bar cost
+}
+
+function Bar(max=30, cost=1, multi=1.2, cmulti=1.375) {
+    this.points = EN(0) //points
+    this.tpoints = EN(0) //total points
+    this.max = EN(max) //maximum value
+    this.level = EN(0) //level
+    this.cost = EN(cost) //multiplier cost (buy)
+    this.multi = EN(multi) //speed multiplier (buy)
+    this.cmulti = EN(cmulti) //cost multiplier (buy)
+    this.speed = EN(1) //speed
+    this.nspeed = EN(1) //normal speed
+    this.pmulti = EN(1) //previous multiplier
+}
+
+//game build
+var game = new Game
+
+function update() {
+    game.bars.forEach((v,i,a)=>{
+        try {
+            game.bars[i].speed = v.nspeed.mul(a[i+1].pmulti)
+        } catch {
+            game.bars[i].speed = v.nspeed
+        }
+    })
+    $('points').innerHTML = 'Points: ' + ts(game.points)
+    $('next-bar').innerHTML = 'Buy next progress bar | Cost: ' + ts(game.next)
+    for (var i in game.bars) {
+        $('prog-bar-' + i).style.width = (game.bars[i].points.div(game.bars[i].max) * 100) + '%'
+        $('prog-text-' + i).innerHTML = ts(EN.floor(game.bars[i].points)) + ' / ' + ts(EN.floor(game.bars[i].max)) + '\nMulti: ' + ts(game.bars[i].speed) + ', Up: x' + ts(game.bars[i].pmulti)
+        $('prog-level-' + i).innerHTML = 'Level\n' + ts(game.bars[i].level)
+        $('prog-buy-' + i).innerHTML = 'Upg. for:\n' + ts(EN.ceil(game.bars[i].cost))
+    }
+}
+
+function barIncrement(id) {
+    game.bars.forEach((v,i,a)=>{
+        try {
+            game.bars[i].speed = v.nspeed.mul(a[i+1].pmulti)
+        } catch {
+            game.bars[i].speed = v.nspeed
+        }
+    })
+    game.bars.forEach((v,i)=>{game.bars[i].pmulti = i != 0 ? v.level.pow(0.715).div(20).add(1) : EN(1)})
+    game.bars[id].points = game.bars[id].points.add(game.bars[id].speed)
+    game.bars[id].tpoints = game.bars[id].tpoints.add(game.bars[id].speed)
+    if (id == 0) {
+        game.points = game.points.add(EN.floor(game.bars[id].points.div(game.bars[id].max)).mul(game.bars[0].pmulti))
+        game.tpoints = game.tpoints.add(EN.floor(game.bars[id].points.div(game.bars[id].max)).mul(game.bars[0].pmulti))
+    }
+    game.bars[id].level = game.bars[id].level.add(EN.floor(game.bars[id].points.div(game.bars[id].max)))
+    game.bars[id].points = EN.mod(game.bars[id].points, game.bars[id].max)
+    update()
+    new Audio('audio/click.mp3').play()
+}
+function barBuy(id) {
+    if (game.bars[id].level.gte(game.bars[id].cost)) {
+        game.bars[id].cost = game.bars[id].cost.mul(game.bars[id].cmulti)
+        game.bars[id].nspeed = game.bars[id].nspeed.mul(game.bars[id].multi)
+        new Audio('audio/buy.mp3').play()
+        update()
+    } else {
+        new Audio('audio/low.mp3').play()
+    }
+}
+
+function NextBar() {
+    if (game.points.gte(game.next)) {
+        game.points = game.points.sub(game.next)
+        game.next = game.next.mul(16)
+        game.bars.push(new Bar(EN.pow(1.18, game.bars.length).mul(30).floor(), EN.pow(1.2, game.bars.length),
+                               EN.pow(1.05, game.bars.length).mul(1.2), EN.pow(1.05, game.bars.length).mul(1.375)))
+        placeBars()
+        new Audio('audio/buy.mp3').play()
+        update()
+    } else {
+        new Audio('audio/low.mp3').play()
+    }
+}
+
+function fb(i,f) {
+    return EN.div(EN.floor(EN.mul(i,f)),f)
+}
+function ts(i,ep=6,en=-6,f=100,a=0) {
+    var neg = EN.isneg(i)
+    if (neg) i=EN.mul(i,-1)
+    var log = EN.log10(i)
+    if (EN.gte(EN.log10(EN.log10(EN.log10(EN.log10(log)))),0)) {
+        var r = '10^^' + (r2=EN.slog(i),EN.gte(EN.log(r2),ep)?ts(r2,ep,en,f,a+1):fb(r2,f))
+        return neg?'-'+r:r
+    } else {
+        if ((log>=ep | log<=en) & a<10 & !(i==0)) {
+            var r = fb(EN.div(i, EN.pow(10, EN.floor(log))),f)+'e'+ts(EN.floor(log),ep,en,f,a+1)
+            return (neg?EN.mul(r,-1):r).toString()
+        } else {
+            return fb(neg?i*-1:i,f).toString()
+        }
+    }
+}
+
+placeBars()
+update();
+
+(()=>{
+    var a = LdrToRGB(85, game.bars.length / 3.75 - 1.4, 100)
+    ButtonStyle($('next-bar'), [a.r, a.g, a.b])
+    ButtonStyle($('pause'), [255, 255, 255])
+})()
---
+++ games/progress-rise/main.js
@@ -0,0 +1,127 @@
+function $(e) {
+    return document.getElementById(e);
+};
+
+var config = {
+    bars: 0,
+    playing: false,
+}
+
+document.onmousedown = function() { 
+    if (!config.playing) {
+        config.audio = new Audio('audio/themeSong.mp3')
+        config.audio.volume = 0.4
+        config.audio.loop = 'loop'
+        config.audio.play()
+        config.playing = true;
+    }
+}
+
+function LdrToRGB(l, d, r) {
+    var a = Math.sin(d)*r
+    var b = Math.cos(d)*r
+    return LabToRGB(l, a, b)
+}
+function LabToRGB(l, a, b) {
+    var y = (l + 16) / 116,
+    x = a / 500 + y,
+    z = y - b / 200,
+    r, g, b;
+    x = 0.95047 * ((x * x * x > 0.008856) ? x * x * x : (x - 16/116) / 7.787);
+    y = 1.00000 * ((y * y * y > 0.008856) ? y * y * y : (y - 16/116) / 7.787);
+    z = 1.08883 * ((z * z * z > 0.008856) ? z * z * z : (z - 16/116) / 7.787);
+    r = x *  3.2406 + y * -1.5372 + z * -0.4986;
+    g = x * -0.9689 + y *  1.8758 + z *  0.0415;
+    b = x *  0.0557 + y * -0.2040 + z *  1.0570;
+    r = (r > 0.0031308) ? (1.055 * Math.pow(r, 1/2.4) - 0.055) : 12.92 * r;
+    g = (g > 0.0031308) ? (1.055 * Math.pow(g, 1/2.4) - 0.055) : 12.92 * g;
+    b = (b > 0.0031308) ? (1.055 * Math.pow(b, 1/2.4) - 0.055) : 12.92 * b;
+    return {r: Math.max(0, Math.min(1, r)) * 255, 
+            g: Math.max(0, Math.min(1, g)) * 255, 
+            b: Math.max(0, Math.min(1, b)) * 255}
+}
+function color(v=-1.1, b=0, id=0) {
+    var x = Math.sin(v)*100
+    var y = Math.cos(v)*100
+    var c = LabToRGB(86+b, x, y)
+    var c2 = LabToRGB(75+b, x, y)
+    $('prog-cont-'+id).style.backgroundImage = `linear-gradient(rgb(${c.r}, ${c.g}, ${c.b}) 0%,rgb(${c2.r}, ${c2.g}, ${c2.b}) 100%)`
+    c = LabToRGB(38+b, x, y)
+    c2 = LabToRGB(29+b, x, y)
+    $('prog-bar-'+id).style.backgroundImage = `linear-gradient(rgb(${c.r}, ${c.g}, ${c.b}) 0%,rgb(${c2.r}, ${c2.g}, ${c2.b}) 100%)`
+    c = LabToRGB(65+b, x, y)
+    c2 = LabToRGB(52+b, x, y)
+    $('prog-button-'+id).style.backgroundImage = `linear-gradient(rgb(${c.r}, ${c.g}, ${c.b}) 0%,rgb(${c.r}, ${c.g}, ${c.b}) 50%,rgb(${c2.r}, ${c2.g}, ${c2.b}) 51%)`
+    c = LabToRGB(27+b, x, y)
+    $('prog-button-'+id).style.borderColor = `rgb(${c.r}, ${c.g}, ${c.b})`
+    c = LabToRGB(16+b, x, y)
+    $('prog-cont-'+id).style.outline = `4px solid rgb(${c.r}, ${c.g}, ${c.b})`
+    c = LabToRGB(86+b, x, y)
+    c2 = LabToRGB(75+b, x, y)
+    $('prog-level-'+id).style.backgroundImage = `linear-gradient(rgb(${c.r}, ${c.g}, ${c.b}) 0%,rgb(${c2.r}, ${c2.g}, ${c2.b}) 100%)`
+    c = LabToRGB(16+b, x, y)
+    $('prog-level-'+id).style.outline = `4px solid rgb(${c.r}, ${c.g}, ${c.b})`
+    c = LabToRGB(86+b, x, y)
+    c2 = LabToRGB(75+b, x, y)
+    $('prog-buy-'+id).style.backgroundImage = `linear-gradient(rgb(${c.r}, ${c.g}, ${c.b}) 0%,rgb(${c2.r}, ${c2.g}, ${c2.b}) 100%)`
+    c = LabToRGB(16+b, x, y)
+    $('prog-buy-'+id).style.borderColor = `rgb(${c.r}, ${c.g}, ${c.b})`
+
+}
+function rainbow() {
+    a=0
+    b=0
+    int = setInterval(()=>{
+        color(a, b, 0)
+        a+=0.05
+    }, 33)
+}
+document.makeElement = (tag, innerHTML)=>{
+    var node = document.createElement(tag);
+    var textnode = document.createTextNode(innerHTML);
+    node.appendChild(textnode);
+    return node
+}
+document.placeElement = (node, id)=>{
+    document.getElementById(id).appendChild(node);
+}
+function placeBars() {
+    while (config.bars < game.bars.length) {
+        var e = $('progressbar-copy').cloneNode(true)
+        e.style.display = 'block'
+        e.id = 'prog-'+config.bars
+        var c = e.querySelectorAll('.select')
+        c[0].id = 'prog-button-'+config.bars
+        c[1].id = 'prog-cont-'+config.bars
+        c[2].id = 'prog-bar-'+config.bars
+        c[3].id = 'prog-text-'+config.bars
+        c[4].id = 'prog-level-'+config.bars
+        c[5].id = 'prog-buy-'+config.bars
+
+        c[0].addEventListener('click', new Function('barIncrement(' + config.bars + ')'))
+        c[5].addEventListener('click', new Function('barBuy(' + config.bars + ')'))
+
+        document.placeElement(e, 'progressbar-place')
+        color(game.bars.length/3.75-1.4, 0, config.bars)
+        config.bars++
+    }
+}
+function ButtonStyle(node, rgb) {
+    if (node.tagName == 'BUTTON') {
+        node.style.height = '48px'
+        node.style.borderRadius = '0'
+        node.style.borderWidth = '4px'
+        node.style.fontFamily = 'myFirstFont'
+        node.style.whiteSpace = 'pre-wrap'
+        node.style.borderColor = 'rgb('+(rgb[0]/5)+','+(rgb[1]/5)+','+(rgb[2]/5)+')'
+        node.style.backgroundImage = `linear-gradient(rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]}) 0%, rgb(${rgb[0]/1.1}, ${rgb[1]/1.15}, ${rgb[2]/1.15}) 100%)`
+    } else {throw Error('Element is not a button.')}
+}
+
+function mute() {
+    if (config.audio.paused) {
+        config.audio.play()
+    } else {
+        config.audio.pause()
+    }
+}

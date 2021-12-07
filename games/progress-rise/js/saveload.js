0714cc0f34008fea41a93f850916a3efdd7afaa6
BonesYT <63462200+BonesYT@users.noreply.github.com>
2021-12-07 16:34:35 -0300

    committing 26

--- games/progress-rise/js/saveload.js
+++ games/progress-rise/js/saveload.js
@@ -1,8 +1,6 @@
 function save(isCookie=false) {
     if (isCookie) {
-        var date = new Date();
-        date.setTime(date.getTime() + 8.64e10);
-        document.cookie = 'GameData=' + JSON.stringify(game) + '; expires=' + date.toUTCString() + '; path=/';
+        localStorage.setItem('ProgressRise', btoa(JSON.stringify(game)))
     } else {
         $('savecode').value = btoa(JSON.stringify(game))
     }
@@ -10,8 +8,8 @@ function save(isCookie=false) {
 
 function load(isCookie=false) {
     if (isCookie) {
-        if (getCookieValue('GameData') == null) return false
-        var game2 = JSON.parse(getCookieValue('GameData'))
+        if (localStorage.getItem('ProgressRise') == null) return false
+        var game2 = JSON.parse(atob(localStorage.getItem('ProgressRise')))
     }
     else {
         var game2 = JSON.parse(atob($('savecode').value))
@@ -98,17 +96,6 @@ function load(isCookie=false) {
 
     var a = LdrToRGB(85, game.bars.length / 3.75 - 1.4, 100)
     ButtonStyle($('next-bar'), [a.r, a.g, a.b])
-}
-
-function getCookieValue(name) {
-    var nameEQ = name + '=';
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
 }
 
 load(true)

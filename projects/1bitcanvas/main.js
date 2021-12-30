18b988a350eed6bccb45b3a6a8ab0dfcb6a1ac0d
BonesYT <63462200+BonesYT@users.noreply.github.com>
2021-12-29 21:59:38 -0300

    committing 35

--- projects/1bitcanvas/main.js
+++ projects/1bitcanvas/main.js
@@ -67,6 +67,14 @@ var sys = {
         sys.grid.h = Math.max(Math.min($('h').value, 256), 1)
         sys.createCanvas()
     },
+    getHash: ()=>{
+        ecode.value = new URL(document.location.href).hashToString()
+    },
+    setHash: i=>{
+        var e = new URL(document.location.href)
+        e.stringToHash(String(i))
+        document.location.href = e
+    },
     optsOn: false,
     isPlay: false,
     tick: 0,
@@ -88,7 +96,11 @@ var canvas = $('canvas'),
 
 sys.createCanvas()
 
-$('canvas').addEventListener('mousemove', e=>{
+canvas.addEventListener('mousemove', e=>{
     sys.cursor.x = e.offsetX
     sys.cursor.y = e.offsetY
 })
+
+URL.prototype.hashToString=function(){var e=this.hash.substring(1);e=e.replace(/%5B/g,"[");e=e.replace(/%5D/g,"]");e=e.replace(/%7B/g,"{");e=e.replace(/%7D/g,"}");e=e.replace(/%5E/g,"^");e=e.replace(/%60/g,"`");e=e.replace(/%40/g,"@");e=e.replace(/%23/g,"#");e=e.replace(/%24/g,"$");e=e.replace(/%25/g,"%");e=e.replace(/%26/g,"&");e=e.replace(/%2C/g,",");e=e.replace(/%3A/g,":");e=e.replace(/%3B/g,";");e=e.replace(/%2F/g,"/");e=e.replace(/%3F/g,"?");e=e.replace(/%5C/g,"\\");e=e.replace(/%7C/g,"|");e=e.replace(/%20/g," ");e=e.replace(/\+/g," ");return e};URL.prototype.stringToHash=function(e,r){var a=e;a=a.replace(/\[/g,"%5B");a=a.replace(/]/g,"%5D");a=a.replace(/{/g,"%7B");a=a.replace(/}/g,"%7D");a=a.replace(/\^/g,"%5E");a=a.replace(/`/g,"%60");a=a.replace(/@/g,"%40");a=a.replace(/#/g,"%23");a=a.replace(/\$/g,"%24");a=a.replace(/%/g,"%25");a=a.replace(/&/g,"%26");a=a.replace(/,/g,"%2C");a=a.replace(/:/g,"%3A");a=a.replace(/;/g,"%3B");a=a.replace(/\//g,"%2F");a=a.replace(/\?/g,"%3F");a=a.replace(/\\/g,"%5C");a=a.replace(/\|/g,"%7C");a=a.replace(/ /g,r?"+":"%20");this.hash="#"+a};
+
+sys.getHash()

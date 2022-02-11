227beb5e56ef60f7707f214005853a9bcc51ef2d
BonesYT <63462200+BonesYT@users.noreply.github.com>
2022-02-11 16:41:04 -0300

    commiting 49

--- projects/effects/main.js
+++ projects/effects/main.js
@@ -2,7 +2,7 @@ function $(e) {
     return document.getElementById(e)
 }
 
-var imgl = ['BADOOF','Discord[error]','DISGUISED','Lines','RGB spiral','sadcube','Thumbnail44','Website preview','youtube (v2)','ytpost2','[TEST]'],
+var imgl = ['BADOOF','Discord[error]','DISGUISED','Lines','RGB spiral','sadcube','Thumbnail44','Website preview','youtube (v2)','ytpost2','mrIncredible','oof','[TEST]'],
     imgf = imgl.map(v => `images/${v}.png`),
     select = 4,
     mouseX, mouseY,
@@ -11,7 +11,33 @@ var imgl = ['BADOOF','Discord[error]','DISGUISED','Lines','RGB spiral','sadcube'
     width, height,
     img, debug = document.location.href[0] == 'f', data, slidershow = false,
     f = ()=>{}, tick = 0, playing = false, int, err = false,
-    values = [0,0,0,0,0], before, change = true
+    values = [0,0,0,0,0], before, change = true, size = 3,
+    exs = {
+'Brightness': `v[0] += (a + d) * (e / 127.5 - 1);
+v[1] += (b + d) * (e / 127.5 - 1);
+v[2] += (c + d) * (e / 127.5 - 1);
+return v`,
+'Shadow Cursor': `function z(v) {
+    return v - Math.sqrt((x - mx) ** 2 + (y - my) ** 2)
+}
+v[0]=z(v[0]);
+v[1]=z(v[1]);
+v[2]=z(v[2]);
+return v`,
+'Waves': `return getPixel(
+    x+(y*a/255+c/(255/(Math.PI*2))).sin()*b/8,
+    y+(x*d/255+f/(255/(Math.PI*2))).sin()*e/8
+)`,
+'Multi Effects': `var effects = [
+  ()=>{
+    v[0] *= (a / 150)
+  },()=>{
+    v[3] = 255 * (b / 256) + v[3] * (1 - b / 256)
+  }
+]
+effects.forEach(v => {v()})
+return v`
+    }
 
 ;(()=>{
 var a
@@ -24,7 +50,7 @@ for (var i = 0; i < imgl.length + debug - 1; i++) {
         selectimg(select)
     })
     $('imglist').appendChild(a)
-    if (i == 4) {
+    if (i % 5 == 4) {
         $('imglist').appendChild(document.createElement('br'))
     }
 }
@@ -47,7 +73,7 @@ function selectimg(id) {
 selectimg(4)
 
 function getPixel(x, y, typ='rgb') {
-    var i = (x % canvas.width) + (y % canvas.height) * canvas.width
+    var i = (Math.floor(x) % canvas.width) + (Math.floor(y) % canvas.height) * canvas.width
     return [before[i * 4], before[i * 4 + 1], before[i * 4 + 2], before[i * 4 + 3]].conv(typ)
 }
 function setcursor(event) {
@@ -70,7 +96,7 @@ function ontick() {
     }
 
     try {
-        f = new Function('x','y','t','ti','mx','my','v','a','b','c','d','e', $('code').value)
+        f = new Function('x','y','t','ti','mx','my','v','a','b','c','d','e','f', $('code').value)
     } catch (e) {
         if (err != e.toString()) console.log(e.toString())
         err = e.toString()
@@ -83,13 +109,16 @@ function ontick() {
         change = false
     }
 
+    size = typ.length
+
     try {
         for (var i = 0; i < canvas.width * canvas.height; i++) {
             c = [data.data[i * 4], data.data[i * 4 + 1], data.data[i * 4 + 2], data.data[i * 4 + 3]]
             if (typ != 'rgb') c = c.conv(typ)
             c = f(i % canvas.width, (i / canvas.width).floor(), tick, time, mouseX, mouseY, c,
-                  values[0], values[1], values[2], values[3], values[4])
+                  values[0], values[1], values[2], values[3], values[4], values[5])
             if (c.isArray()) {
+                if (c.length != size + 1) c.push(255)
                 if (typ != 'rgb') c = c.conv(typ, 1)
                 data.data[i * 4 + 0] = c[0]
                 data.data[i * 4 + 1] = c[1]
@@ -144,10 +173,16 @@ function updatesliders() {
     values[2] = $('sliderC').value.toNumber()
     values[3] = $('sliderD').value.toNumber()
     values[4] = $('sliderE').value.toNumber()
+    values[5] = $('sliderF').value.toNumber()
     $('slidertextA').innerHTML = 'Slider A: ' + values[0]
     $('slidertextB').innerHTML = 'Slider B: ' + values[1]
     $('slidertextC').innerHTML = 'Slider C: ' + values[2]
     $('slidertextD').innerHTML = 'Slider D: ' + values[3]
     $('slidertextE').innerHTML = 'Slider E: ' + values[4]
+    $('slidertextF').innerHTML = 'Slider F: ' + values[5]
 }
 setInterval(updatesliders, 33)
+
+function loadex() {
+    $('code').innerHTML = exs[$('exopt').value]
+}
